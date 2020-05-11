cfb27360209e3c65ce6a0451ac591ae8261dfce7
sasakinzm <sasakinzm@gmail.com>
2020-05-11 09:12:13 +0900

    complete project 4-1

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,15 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @SCREEN
+    M=1
+    A=A+1
+    M=1
+    A=A+1
+    M=1
+    A=A+1
+    M=1
+    @LOOP
+    0;JMP
