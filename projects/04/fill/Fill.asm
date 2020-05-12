404c35bd85e41aecba8d1c654f1f23bdea766ea4
sasakinzm <sasakinzm@gmail.com>
2020-05-12 23:06:53 +0900

    complete project 4-2

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -13,14 +13,75 @@
 
 // Put your code here.
 
-(LOOP)
-    @SCREEN
-    M=1
-    A=A+1
-    M=1
-    A=A+1
-    M=1
-    A=A+1
-    M=1
-    @LOOP
+(START)
+    // read keyboard
+    @24576
+    D=M
+
+    // if keyboard pushed then goto ON
+    // else goto OFF
+    @ON
+    D;JGT
+    @OFF
+    D;JEQ
+
+(ON)
+    // set Mem[COLOR] to black
+    @COLOR
+    M=-1
+    // goto DRAW
+    @DRAW
     0;JMP
+
+(OFF)
+    // set Mem[COLOR] to white
+    @COLOR
+    M=0
+    // goto DRAW
+    @DRAW
+    0;JMP
+
+(DRAW)
+    // set Mem[COUNT] == 8192
+    @8192
+    D=A
+    @COUNT
+    M=D
+
+    // set Mem[NOW] == 16384
+    // NOW indicates the pixel that is currently being painted.
+    @SCREEN
+    D=A
+    @NOW
+    M=D
+
+    // start drawing loop
+(LOOP)
+    // set color
+    @COLOR
+    D=M
+    // paint pixel
+    @NOW
+    A=M
+    M=D
+    // slide NOW to next pixel
+    @NOW
+    M=M+1
+    // reduce counter
+    @COUNT
+    M=M-1
+    // if D > 0 then goto loop
+    // elif D == 0 then goto START
+    D=M
+    @LOOP
+    D;JGT
+    @START
+    D;JEQ
+
+
+
+
+
+
+
+
